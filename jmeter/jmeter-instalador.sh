3aff2e8a959e00aa7ef09174851e72c30f212afd
jvm <jvm@localhost.localdomain>
2023-11-23 12:12:26 +0100

    Corrección del instalador de jmeter

--- jmeter/jmeter-instalador.sh
+++ jmeter/jmeter-instalador.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 wget https://dlcdn.apache.org//jmeter/binaries/apache-jmeter-5.6.2.tgz
 set -e
-sha512sum --check apache-jmeter-5.6.2.tgz-sha512
+sha512sum --check apache-jmeter-5.6.2.tgz.sha512
 tar -xvf apache-jmeter-5.6.2.tgz
